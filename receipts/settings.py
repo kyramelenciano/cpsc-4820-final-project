bf3e4d2210c2e7afb85bd1799e6711731279a79b
Kyra Melenciano <melencianokyra@gmail.com>
2022-11-30 11:21:44 -0800

    changing database to sqlite

--- receipts/settings.py
+++ receipts/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'receipts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lenin',
-        'USER': 'lenin',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
