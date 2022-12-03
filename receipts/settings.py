a75d6c805d2b55d5a7a9aeae5b7e5b9a1b733d01
Kyra Melenciano <melencianokyra@gmail.com>
2022-12-03 11:40:01 -0800

    add jazzmin custom admin panel

--- receipts/settings.py
+++ receipts/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'receipts_processing.apps.ReceiptsProcessingConfig',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
